cb4c72022f8a3a28a56bac3a96065a97b2733327
Sam Taylor <2015samtaylor@gmail.com>
2025-07-22 19:02:11 -0500

    Release version 0.1.4

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="gcp_utils_sds",
+    version="0.1.4",
     packages=find_packages(),  # This should find gcp_utils_sds
     install_requires=[
         "google-cloud-storage",
